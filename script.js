08985cca8e2c9551f6ba6d5586a66cfd7973d094
SimaoBarbosaOrg <simaobarbosa132005@gmail.com>
2026-02-28 17:12:32 +0000

    Imagens fix

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const images = [
-  "images/image1.jpg",
-  "images/image2.jpg",
-  "images/image3.jpg",
-  "images/image4.jpg",
-  "images/image5.jpg",
-  "images/image6.jpg"
+  "images/image1.png",
+  "images/image2.png",
+  "images/image3.png",
+  "images/image4.png",
+  "images/image5.png",
+  "images/image6.png"
 ];
 
 function showImages() {
